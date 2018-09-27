90615cace261ff8d1c0fe168ae24b03a25bebcc2
ningjianhao <375332835@qq.com>
2018-09-27 12:53:31 +0800

    commit

--- src/store/getters.js
+++
@@ -1,6 +0,0 @@
-const getters = {
-  tableData: state => state.user.tableData,
-  signTableData: state => state.user.signTableData,
-  feeData: state => state.user.feeData
-}
-export default getters
--- static/walletConfig.js
+++ static/walletConfig.js
@@ -1,11 +1,5 @@
 
 const urlData = {
-  url: 'http://192.168.15.31:8099', // 'http://localhost:8099',
-  ossObj: {
-    accessid: 'LTAIWG3fwPIlG6Qh',
-    dir: 'tokenExchange',
-    expire: '1522121349',
-    host: 'http://buytoken.oss-cn-hongkong.aliyuncs.com'
-  }
+  url: 'http://localhost:8099',
 }
 window.urlData = urlData
